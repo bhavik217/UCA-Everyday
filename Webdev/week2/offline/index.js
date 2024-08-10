fb431df51215f759e1d02d1ed4ade6660a30b2a1
bhavik217 <bhavik217.be22@chitkara.edu.in>
2024-08-10 11:30:44 +0530

    Add files via upload

--- Webdev/week2/offline/index.js
+++ Webdev/week2/offline/index.js
@@ -1,24 +1,23 @@
-// function init() {
-//     document.getElementById("jsworld").innerHTML = "Hello World from JavaScript!";
-// }
+function renderProducts() {
+    var productsListFromServer;
 
+    function fetchProductList() {
+        console.log("Fetching product list from server");
+        // Assuming `productsList` is a global variable or fetched from a server.
+        productsListFromServer = productsList; 
+        initProductItem();
+    }
 
-var productsList = []
+    function initProductItem() {
+        console.log("Initializing product items HTML Table");
+        if (!productsListFromServer || productsListFromServer.length === 0) {
+            document.getElementById("productMenu").innerHTML = getLoader();
+            return;
+        }
 
-function fetchProductList() {
-    console.log("Fetching product list from server")
-    productsList = [
-        {name: "Product 1", description: "Some additional details about P1", price: 100},
-        {name: "Product 2", description: "Some additional details about P2", price: 200}
-    ]
-    initProductItem();
-}
-
-function initProductItem() {
-    console.log("Initializing product items HTML Table")
-    document.getElementById("productMenu").innerHTML = `
-    <h3>List of Products</h3>
-    <table>
+        document.getElementById("productMenu").innerHTML = `
+        <h3>List of Products</h3>
+        <table>
             <thead>
                 <tr>
                     <th>Serial No.</th>
@@ -30,45 +29,24 @@ function initProductItem() {
             <tbody>
                 ${getProductListUsingMap()}
             </tbody>
-        </table>
-    `;
-}
-
-var random_arr = [1, 2];
-
-function getProductList() {
-
-    var rows = "";
-    for (let i = 0; i < productsList.length; i++ ) {
-
-        rows = rows + `
-        <tr>
-        <td>${i+1}</td>
-        <td>${productsList[i].name}</td>
-        <td>${productsList[i].description}</td>
-        <td>${productsList[i].price}</td>
-        </tr>`
+        </table>`;
     }
-    return rows;
+
+    function getLoader() {
+        return `<h1>Loading Data</h1>`;
+    }
+
+    function getProductListUsingMap() {
+        return productsListFromServer.map((item, index) => `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${item.name}</td>
+                <td>${item.description}</td>
+                <td>${item.price}</td>
+            </tr>`).join("");
+    }
+
+    setTimeout(fetchProductList, 1000);
 }
 
-function getProductListUsingMap() {
-
-    var rowsOfProdcuts = productsList.map((item, index) => {
-        return `
-        <tr>
-        <td>${index+1}</td>
-        <td>${item.name}</td>
-        <td>${item.description}</td>
-        <td>${item.price}</td>
-        </tr>`
-    })
-
-    return rowsOfProdcuts.join("");
-}
-
-// init();
-
-initProductItem();
-setTimeout(fetchProductList, 1000)
-// fetchProductList();
+renderProducts();
