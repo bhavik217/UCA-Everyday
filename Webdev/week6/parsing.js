384536c45f000c46011c0c30e77618a5f814f86f
bhavik217 <bhavik217.be22@chitkara.edu.in>
2024-08-31 06:58:09 +0530

    Add files via upload

---
+++ Webdev/week6/parsing.js
@@ -0,0 +1,24 @@
+let obj = { "name": "Singla", "age": 19, "city": "Patiala" };
+
+// Accessing the value one by one like this is called parsing
+console.log(obj.name);
+
+// Parsing the object
+console.log(obj);
+
+// Parsing the object in a better way
+console.log(JSON.stringify(obj));
+
+// Parsing the object in a better way with indentation
+console.log(JSON.stringify(obj, null, 2));
+
+
+// Destructuring the object into variables
+let {"name": name1, "age": age1, "city": city1} = obj;
+
+// Printing the destructured object
+console.log(name1);
+console.log(age1);
+console.log(city1);
+
+console.log(obj);
