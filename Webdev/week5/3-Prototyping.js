b1537bbcab3c3dc506d6d2c1ff20fb6d12654bed
bhavik217 <bhavik217.be22@chitkara.edu.in>
2024-08-31 06:18:16 +0530

    Updated week5

--- Webdev/week5/3-Prototyping.js
+++ Webdev/week5/3-Prototyping.js
@@ -1,89 +1,28 @@
-// Prototyping Inheritence
-// Achieved through Prototype Chaining Method
-
-function student(fName, obtainedMarks) {
-    
-    this.fNameObj = fName;
-    this.obtMarksObj = obtainedMarks;
-    this.checkResults =  function() {
-            
-            let percentage = ( this.obtMarksObj / maxMarks ) * 100;
-            
-            if(percentage >= 40) {
-                return "Passed";
-            } else {
-                return "Failed";
-        }
-    }
+function Person(name) {
+    this.name = name;
 }
 
-var student1 = new student("Harshit", 200); 
+// Add a method to the prototype
+Person.prototype.greet = function() {
+    return "Hello, my name is " + this.name;
+};
 
-student.prototype.college = "Chitkara"; 
-student.prototype.maxMarks = 500;
+var person1 = new Person("Alice");
 
-student.prototype.mathMaxMarks = 100;
+// Access the prototype method through the instance
+console.log(person1.greet());  // Output: "Hello, my name is Alice"
 
-console.log(student1);
-console.log(student1.__proto__);
-console.log(student1.mathMaxMarks);
-console.log(typeof student1);
+// Create another object that inherits from person1
+var person2 = Object.create(person1);
 
+// person2 now has access to the greet method from person1's prototype
+console.log(person2.greet());  // Output: "Hello, my name is Alice"
 
-// student1 will have all the properties as per normal Data Structures have
-console.log(student1.toString())
+// Modify the prototype property
+Person.prototype.greet = function() {
+    return "Hi, I'm " + this.name;
+};
 
-student1.__proto__.mathMaxMarks = 1000;
-// It will even modify the parent class protoype
-
-console.log(student1.mathMaxMarks); 
-console.log(student.prototype.mathMaxMarks);
-
-/* -------------------------------------------------------------------------------------- */
-/* Inheritence Prototype */
-
-var arr = [1, 2, 3]
-
-callBackFunction1 = function(value) {
-    console.log(value);
-}
-
-// arr.forEach(callBackFunction1);
-// Let's create arr.forEachCustom
-
-Array.prototype.forEachCustom = function(callBackFunction) {
-    for(let i = 0; i < this.length; i++) {
-        callBackFunction(this[i]); // this refers to the array
-    }
-}
-
-arr.forEachCustom(callBackFunction1);
-
-Array.prototype.multiplyByTwo = function() {
-    for(let i = 0; i < this.length; i++) {
-        this[i] = this[i] * 2;
-    }
-    return this;
-}
-
-multiplyByTwoCallback = function(value) {
-    return value * 2;
-}
-
-console.log(arr.multiplyByTwo());
-
-
-// Map will not modify the original array but will return a new array with the modified values
-Array.prototype.mapCustom = function(callBackFunction) {
-    let result = []
-    for (let i = 0; i < this.length; i++)
-    result.push(callBackFunction(this[i]));
-
-    return result;
-}
-
-
-console.log(arr.mapCustom(multiplyByTwoCallback));
-
-
-/* -------------------------------------------------------------------------------------- */
+// The change is reflected in all instances and objects that share the prototype
+console.log(person1.greet());  // Output: "Hi, I'm Alice"
+console.log(person2.greet());  // Output: "Hi, I'm Alice"
